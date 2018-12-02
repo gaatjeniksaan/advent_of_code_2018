efaab3c6672f269b6c03239e48baf76c73159e18
barendlinders <barendlinders@gmail.com>
2018-12-02 08:50:30 +0100

    part 2 in python - tad ugly

--- day2/day2.py
+++ day2/day2.py
@@ -10,5 +10,32 @@ def count_letter_occurence(seq):
 counter = Counter()
 for seq in data:
 	counter.update(count_letter_occurence(seq))
-
 print("Answer part 1: {}".format(counter[2] * counter[3]))
+
+# Part 2
+def find_duplicate(seq, data):
+	data_copy = data.copy()
+	data_copy.remove(seq)  # We don't want to match with the input itself
+
+	for d in data_copy:
+		diff = 0
+		for index, char in enumerate(d):
+			if char != seq[index]:
+				diff += 1
+
+		if diff	== 1:
+			return seq, d
+	
+	return None, None
+
+count = 0
+for d in data:
+	a, b = find_duplicate(d, data)
+	if a:
+		answer = ""
+		for index, char_a in enumerate(a):
+			if char_a == b[index]:
+				answer += char_a
+		print("Answer part 2: {}".format(answer))
+		break
+
