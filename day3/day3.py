74488bd5e912dd4494355c9a8e7cd6ca4e360168
barendlinders <barendlinders@gmail.com>
2018-12-03 10:59:28 +0100

    day3 part 2 in python

--- day3/day3.py
+++ day3/day3.py
@@ -8,20 +8,27 @@ with open("./data.txt") as f:
 		"width": 	int(x.split(" ")[3].split("x")[0]),
 		"length":	int(x.split(" ")[3].split("x")[1].strip("\n")),}
 		for x in f.readlines()]
-# print(data)
 
 coords = defaultdict(int)
 
 # Loop over dicts and add all coordinates to coords
-for item in data:
-	# Get upper right coordinate first
-	ul_row = item["top"]
-	ul_col = item["left"]
+for i in range(2):
+	for item in data:
+		overlap = False
+		# Get upper right coordinate first
+		ul_row = item["top"]
+		ul_col = item["left"]
 
-	for row in range(item["length"]):
-		for col in range(item["width"]):
-			r = ul_row + row
-			c = ul_col + col
-			coords[(r, c)] += 1
+		for row in range(item["length"]):
+			for col in range(item["width"]):
+				r = ul_row + row
+				c = ul_col + col
+				if i == 1 and coords[(r, c)] > 1:
+					overlap = True
+				coords[(r, c)] += 1
 
-print("Answer part 1: ", sum(1 for i in coords.values() if i > 1))
+		if i == 1 and overlap == False:
+			print("Answer part 2: ", item["id"].strip("#"))
+
+	if i == 0:
+		print("Answer part 1: ", sum(1 for i in coords.values() if i > 1))
