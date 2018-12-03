64043715b09f00951543df5cc34d3f821952620e
barendlinders <barendlinders@gmail.com>
2018-12-03 21:12:09 +0100

    day3 pt2 in python fix

--- day3/day3.py
+++ day3/day3.py
@@ -2,17 +2,18 @@ from collections import defaultdict
 
 with open("./data.txt") as f:
 	data = [{
-		"id":		x.split(" ")[0],
-		"left": 	int(x.split(" ")[2].split(",")[0]),
-		"top":		int(x.split(" ")[2].split(",")[1].strip(":")),
-		"width": 	int(x.split(" ")[3].split("x")[0]),
-		"length":	int(x.split(" ")[3].split("x")[1].strip("\n")),}
+		"id":		x.split()[0],
+		"left": 	int(x.split()[2].split(",")[0]),
+		"top":		int(x.split()[2].split(",")[1].strip(":")),
+		"width": 	int(x.split()[3].split("x")[0]),
+		"length":	int(x.split()[3].split("x")[1].strip("\n")),}
 		for x in f.readlines()]
 
 coords = defaultdict(int)
 
-# Loop over dicts and add all coordinates to coords
 for i in range(2):
+	# First loop to check populate coordinates and check how many are doubled
+	# Second loop to check per ID if they access a coordinate that is a double
 	for item in data:
 		overlap = False
 		# Get upper right coordinate first
@@ -25,6 +26,7 @@ for i in range(2):
 				c = ul_col + col
 				if i == 1 and coords[(r, c)] > 1:
 					overlap = True
+					continue
 				coords[(r, c)] += 1
 
 		if i == 1 and overlap == False:
@@ -32,3 +34,4 @@ for i in range(2):
 
 	if i == 0:
 		print("Answer part 1: ", sum(1 for i in coords.values() if i > 1))
+		print("Length of coords: ", len(coords))
